24de8dcb9d05275dc95f1e34f2c8bebed3fadac1
ozzyjohnson <ozzy.johnson@gmail.com>
2014-09-04 20:42:25 -0400

    getting started

---
+++ ait.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+mkdir
+import subprocess
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', nargs ='*')
+args = parser.parse_args()
+
+# Parses instance-ids out of describe-instances input.
+command1 = ["jq", "-r", ".Reservations[].Instances[].InstanceId"]
+
+# Basic xargs with a replacement string.
+command2 = ["xargs", "-I", "%", "aws", "ec2", "create-tags", "--resources", "%", "--tags"]
+
+# Make a list of lists out of the k:v pairs on the command line.
+kvPairs = []
+for kvPair in args.t:
+        kvPairs.append(kvPair.split(':'))
+
+# Append to the xargs command.
+for pair in kvPairs:
+    command2.append('Key=' + str(pair[0]) + ',' + 'Value=' + str(pair[1]))
+
+# Pipe everything together and print the result.
+p1 = subprocess.Popen(command1, stdout=subprocess.PIPE)
+p2 = subprocess.Popen(command2, stdin=p1.stdout, stdout=subprocess.PIPE)
+p1.stdout.close()
+output,err = p2.communicate()
+print output
